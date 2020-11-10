d4f94bb31b8ee3343b49e88b86a1686b4607d130
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2020-11-10 15:29:25 +0100

    Bookings Controller: defined new, view rendered in book#show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,11 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.where(user_id: current_user.id)
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :books
+  resources :books do
+    resources :bookings, only: [:new, :create]
+  end
 
   resources :users do
     resources :bookings, only: [:index]
