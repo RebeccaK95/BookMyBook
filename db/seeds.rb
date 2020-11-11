37977ea8358ed995fb14e924d95aa22a9cebe4ae
Rebecca Kornwolf <rebeccakornwolf@gmail.com>
2020-11-11 15:09:17 +0100

    deleted seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts " Creating books"
-
-5.times do
-  Book.create(
-    title: Faker::Book.title,
-    author: Faker::Book.author,
-    address: Faker::Address.city,
-    category: Faker::Book.genre,
-    user_id: (1..2).to_a.sample
-    )
-end
-
-puts "done"
-
-
-puts " Creating bookings"
-
-5.times do
-  Booking.create(
-    user_id: (1..2).to_a.sample,
-    book_id: (3..5).to_a.sample,
-    start_date: Date.today,
-    end_date: Date.new('2020-12-11')
-    )
-end
-
-puts "done"
-
