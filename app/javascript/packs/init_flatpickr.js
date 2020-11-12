90552be84b8418d91b7bfaa182d4b814f685c0c7
camille colet <camcolet@gmail.com>
2020-11-12 15:05:58 +0100

    calendar working

--- app/javascript/packs/init_flatpickr.js
+++ app/javascript/packs/init_flatpickr.js
@@ -2,8 +2,8 @@ import flatpickr from "flatpickr"
 
 const initFlatpickr = () => {
 
-  const startDateInput = document.querySelector(".booking_start_date");
-  const endDateInput = document.querySelector(".booking_end_date");
+  const startDateInput = document.getElementById('booking_start_date');
+  const endDateInput = document.getElementById('booking_end_date');
 
   // Check that the query selector id matches the one you put around your form.
   if (startDateInput) {
