ec501bb02d7c201d28ef0fbedbfaa55a6a211ac4
camille colet <camcolet@gmail.com>
2020-11-13 13:44:21 +0100

    whislist ok in show

--- app/javascript/packs/heart.js
+++ app/javascript/packs/heart.js
@@ -3,9 +3,8 @@ const heart = () => {
 
 
   heart.addEventListener('click', (event) => {
-    heart.classList.remove("far");
-    heart.classList.add("fas");
-    console.log(heart)
+    heart.classList.toggle("far");
+    heart.classList.toggle("fas");
   })
 }
 
