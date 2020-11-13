b6a376a132738d6a2196f59ecf356fbf6da84e1d
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2020-11-13 11:41:58 +0100

    Error message in calendar fixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,6 @@ class Booking < ApplicationRecord
   validate :end_date_cannot_be_before_start_date
   validate :reservations_must_not_overlap
 
-
   def start_date_cannot_be_in_the_past
     errors.add(:start_date, "Please choose a date from today onwards") if start_date < Date.today
   end
@@ -16,18 +15,17 @@ class Booking < ApplicationRecord
     errors.add(:end_date, "Please choose a later date") if end_date < start_date
   end
 
-private
+  private
 
   def reservations_must_not_overlap
     return if self
-                .class
-                .where.not(id: id)
-                .where(book_id: book_id)
-                .where('start_date <= ? AND end_date >= ?', end_date, start_date)
-                .none?
+              .class
+              .where.not(id: id)
+              .where(book_id: book_id)
+              .where('start_date <= ? AND end_date >= ?', end_date, start_date)
+              .none?
 
-    errors.add(:start_date, 'Overlapping reservation exists')
-    errors.add(:end_date, 'Overlapping reservation exists')
+    errors.add(:start_date, 'This book is not available at the dates you chose. Please update the booking start date and/or end date.')
+    errors.add(:end_date, 'This book is not available at the dates you chose. Please update the booking start date and/or end date.')
   end
-
 end
